1bc5274a70cb4b1196ca29ad6b4ac014c76d585a
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-11-02 15:12:34 +0900

    add subfolders for products

---
+++ pages/products/index.vue
@@ -0,0 +1,32 @@
+<!-- NUXT uses the folder system to create the routes system.
+using index as the file name, the component with that name becomes the root
+of that route
+like this
+this index.vue file is inside the products folder, therefore, it will be shown when the url
+/products get loaded -->
+
+<template>
+
+    <div>
+        <h2>PRODUCTS</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est iusto nihil commodi voluptatibus cumque ab
+            nesciunt doloribus aut, illum tempora deleniti ipsam maiores magnam possimus a libero nemo adipisci
+            pariatur?</p>
+    </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px
+}
+
+p {
+    margin: 20px 0;
+}
+</style>
