5abff7772ee8ca5b7db1f2d356fab6ccdc972793
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2022-11-05 00:12:27 +0900

    use nuxt-link premade component to navigate
    the nuxt-link premade component works just like the router-link in the
    vue-router. It creates an anchor tag but prevent the http request which
    is important to have a more efficient and faster navigation and add
    the classes that can be used to style elements differently, depending
    on what is the current component.

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,22 @@
 <template>
 
     <div>
+        <header>
+            <nav>
+                <nuxt-link to="/">Top</nuxt-link>
+                <ul>
+                    <li>
+                        <nuxt-link to="/">Home</nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link to="/about">About</nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link to="/products">Products</nuxt-link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
         <h2>Home</h2>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est iusto nihil commodi voluptatibus cumque ab
             nesciunt doloribus aut, illum tempora deleniti ipsam maiores magnam possimus a libero nemo adipisci
