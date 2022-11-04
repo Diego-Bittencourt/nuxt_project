154d77298ce8165eff2aebc6f2971fcd10bf451d
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2022-11-04 23:55:04 +0900

    create a dynamic url loading a certain component and get data
    I created a component that would be loaded whenever the url is inside
    localhost/products/<some id>
    
    <some id> is the product's id and it will be grabbed by the component
    and used inside it.
    To achieve that, the dynamic component should be named using square
    brackets with an term inside, like [someid].vue
    
    Inside the component, I can use the destructuring feature to get the
    term in the url, inside the useRoute().params, the <someid> and use
    it inside the component.
    The destructuring must have he same name as the component.

---
+++ pages/products/[id].vue
@@ -0,0 +1,24 @@
+
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, autem ducimus illum praesentium suscipit
+            odio asperiores fugiat sit beatae magni molestias tempora quibusdam accusantium, enim et deleniti earum
+            eveniet excepturi.</p>
+    </div>
+</template>
+    
+    
+    
+    
+<script setup>
+//grabing the id inside the useRoute.parameters from the url.
+//Note that the destructuring proccess must use the same name as the component name.
+const { id } = useRoute().params
+
+</script>
+
+<style  scoped>
+
+</style>
+    
