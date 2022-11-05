bf1a980790cff04101820bc10d7d78ac239ad820
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2022-11-05 23:42:14 +0900

    fetch data for each product using useFetch(link, { key: id })
    I'm fetching data for each product. The useFetch() function avoids
    making a http request everytime the [id] component is used so it
    doesn't work as expected. All product pages show the information for the
    same product, made in the first http request. To make the useFetch()
    perform one request for each product id, I add an object as the second
    argument with a unique id. It differentiate each http request.

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,10 +1,9 @@
 
 <template>
     <div>
-        <p>Product details for {{ id }}</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, autem ducimus illum praesentium suscipit
-            odio asperiores fugiat sit beatae magni molestias tempora quibusdam accusantium, enim et deleniti earum
-            eveniet excepturi.</p>
+        <p>{{ product.title }}</p>
+        <p>{{ product.price }}</p>
+        <p>{{ product.id }}</p>
     </div>
 </template>
     
@@ -16,9 +15,27 @@
 //Note that the destructuring proccess must use the same name as the component name.
 const { id } = useRoute().params
 
+//creating the uri
+const uri = 'https://fakestoreapi.com/products/' + id;
+
+
+//fetching the product info
+const { data: product } = await useFetch(uri, { key: id });
+
+//the useFetch function avoids unnecessary work, therefore, although each product page is being loaded with a different id in the url,
+// the data insid the page is the same from the first http request made.
+//The way around it is to differentiate from each useFetch() by adding a second argument: a option object with the key vlue to a unique term.
+//in this case, the item id works.
+
+
+
+
 definePageMeta({
     layout: 'products'
 })
+
+
+
 </script>
 
 <style  scoped>
