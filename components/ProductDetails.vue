31bd143fd87f4c58c6d2812933fbf993570dfc40
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2022-11-06 23:12:48 +0900

    Add the app object with metatags like description, title and links
     can add metatags, external links and other stuff in the nuxt.config.ts
     file this way.
    inside the defineNuxtConfig export, insert the app value which acceps
    an object
    this object accept the head value  which also expects an object
    inside the head object, I can add the title, in a string and metatags.
    the meta expect an array of objects. each object is a metatag that gets
    the name and the content
    also, inside the head object, I can add external links to stylesheets
    and etc with the link value which expects an array of objects
    each object in the array is a different link with a rel and the href

--- components/ProductDetails.vue
+++ components/ProductDetails.vue
@@ -3,12 +3,19 @@
         <div class="grid grid-cols-2 gap-10">
             <div class="p-7">
                 <img :src="product.image" alt="product img" class="max-auto-my-7">
-            </div> 
+            </div>
             <div class="p-7">
                 <h2 class="text-4xl my-7">{{ product.title }}</h2>
                 <p class="text-xl my-7">Price - ${{ product.price }}</p>
                 <h3 class="font-bold border-b-2 mb-4 pb-2">Product Description</h3>
                 <p class="mb-7">{{ product.description }}</p>
+                <button class="btn flex">
+                    <!-- I added the Material Sybol google Api icons to the metatags in  the nuxt.config.ts file -->
+                    <span class="material-symbols-outlined mr-2">
+                        add_shopping_cart
+                    </span>
+                    <span>Add to Cart</span>
+                </button>
             </div>
         </div>
     </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,36 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ["@nuxtjs/tailwindcss"]
+    modules: ["@nuxtjs/tailwindcss"],
+    app: {
+        head: {
+            title: 'Web Shop',
+            meta: [
+                { name: 'description', content: 'Everything you want can be found here' }
+            ],
+            link: [
+                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200' }
+            ]
+        }
+    }
 })
+
+//I can add metatags, external links and other stuff in the nuxt.config.ts file this way.
+//inside the defineNuxtConfig export, insert the app value which acceps an object
+//this object accept the head value  which also expects an object
+//inside the head object, I can add the title, in a string and metatags.
+//the meta expect an array of objects. each object is a metatag that gets the name and the content
+//also, inside the head object, I can add external links to stylesheets and etc with the link value which expects an array of objects
+//each object in the array is a different link with a rel and the href
+
+//in the snapshot below, Im adding the title, one metatag regarding the descrition of the site and an external link to the Material Symbol stylesheet
+// app: {
+//     head: {
+//         title: 'Web Shop',
+//         meta: [
+//             { name: 'description', content: 'Everything you want can be found here' }
+//         ],
+//         link: [
+//             { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200' }
+//         ]
+//     }
+// }
